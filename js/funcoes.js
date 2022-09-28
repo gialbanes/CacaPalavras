93c166d31592cfd3e4d2213aba51eba56e0ce25e
unknown <suporte@PC19.ACADEMICO>
2022-09-28 11:21:18 -0300

    Versão 1

---
+++ js/funcoes.js
@@ -0,0 +1,23 @@
+
+        matriz = new Array;
+        matriz = [
+        ["W","O","G","E","L","N","N","F","I","W","E","T"],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+            ]
+
+        for(linha=0;linha<13;linha++){
+        for(coluna=0;coluna<13;coluna++){
+        document.write(matriz[linha][coluna]);
+        }
+        document.write("<br>");
+    }
